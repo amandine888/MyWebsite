3d7701f7b83be2cb4eb7fb695da679ba5eb9f477
Amandine <amandine2.divay@gmail.com>
2020-03-10 11:44:34 +0100

    base project

---
+++ front/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Route, Switch, Link, BrowserRouter as Router} from 'react-router-dom'; 
+
+class App extends React.Component {
+  render () {
+    return (
+      <div>
+      </div>
+    )
+  }
+}
+
+export default App;
