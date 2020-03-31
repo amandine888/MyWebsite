301c9f75f861000af0cb363d2dab88b5d32e8172
Amandine <amandine2.divay@gmail.com>
2020-03-31 11:13:24 +0200

    update models Media, Tags & Favorite

--- back/models/favorite.js
+++ back/models/favorite.js
@@ -4,6 +4,23 @@ let favoriteSchema = new mongoose.Schema ({
     total: {
         type: 'number', 
     }, 
+
+    userId: [{
+        type: mongoose.Schema.Types.ObjectId, ref: 'User'
+    }], 
+
+    eventId: [{
+        type: mongoose.Schema.Types.ObjectId, ref: 'Event'
+    }], 
+
+    assoId: [{
+        type: mongoose.Schema.Types.ObjectId, ref: 'Asso'
+    }],
+
+    tagId: [{
+        type: mongoose.Schema.Types.ObjectId, ref: 'Tag'
+    }],
+
 })
 
 module.exports = mongoose.model ('Favorite', favoriteSchema); 
--- back/models/tag.js
+++ back/models/tag.js
@@ -4,6 +4,14 @@ let tagSchema = new mongoose.Schema ({
     libelle: {
         type: 'string', 
     }, 
+
+    eventId: [{
+        type: mongoose.Schema.Types.ObjectId, ref: 'Event'
+    }], 
+
+    assoId: [{
+        type: mongoose.Schema.Types.ObjectId, ref: 'Asso'
+    }],
 })
 
 module.exports = mongoose.model ('Tag', tagSchema); 
