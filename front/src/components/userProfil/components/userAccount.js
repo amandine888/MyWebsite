8cb3ea843b84601e34d27feeff4889a630416d1b
Amandine <amandine2.divay@gmail.com>
2020-06-08 16:05:46 +0200

    integration & css userAccount component

--- front/src/components/userProfil/components/userAccount.js
+++ front/src/components/userProfil/components/userAccount.js
@@ -1,9 +1,16 @@
 import React from 'react'; 
-import { withRouter } from 'react-router-dom';
+import { withRouter, Link } from 'react-router-dom';
 import JwtDecode from 'jwt-decode';
 
 // import child component : 
 import Nav from '../../navBar/components/navBar';
+import Footer from '../../footer/components/Footer'; 
+import UserInfos from '../components/userInfos'; 
+import Favorite from '../components/userFavorite'; 
+
+// import CSS : 
+import '../../../css/Mystyle.css'; 
+import '../css/styleUserprofil.css'; 
 
 // Style : 
 const logStyle = {
@@ -20,6 +27,10 @@ class Useraccount extends  React.Component {
     constructor (props) {
         super (props); 
 
+        this.state = {
+            mode : 'info'
+        }; 
+
         let token = localStorage.getItem('token')
         if (!token)
             this.props.history.push('/'); 
@@ -30,7 +41,22 @@ class Useraccount extends  React.Component {
                 this.props.history.push('/'); 
         }
 
+        this.clickInfo = this.clickInfo.bind(this); 
+        this.clickFavorite = this.clickFavorite.bind(this);
+
     };
+
+    clickInfo (e) {
+        this.setState ({
+            mode: 'info'
+        }); 
+    }
+
+    clickFavorite (e) {
+        this.setState ({
+            mode: 'favorite'
+        }); 
+    }
 
     // componentDidMount (){
     //     let token = localStorage.getItem('token');
@@ -46,14 +72,44 @@ class Useraccount extends  React.Component {
 
     render (){
 
-        return (
-            <div>
-                <Nav/>
-                <div style={div}>
-                    <p style={logStyle}>Hello</p>
+        if (this.state.mode === 'info') {
+            return (
+                <div>
+                    <Nav/>
+                    <div className="headerContainerBis">
+                        <h2>Compte Personnel</h2>
+                    </div>
+                    <div className='menuPerso'>
+                        <button className='selectInfo' type='button' onClick={this.clickInfo}>Informations personnelles</button>
+                        <button className='buttonFavorite' type='button' onClick={this.clickFavorite}>Favoris</button>
+                    </div>
+                    <UserInfos/>
+                    {/* <div style={div}>
+                        <p style={logStyle}>Hello</p>
+                    </div> */}
+                    <Footer/>
                 </div>
-            </div>
-        )
+            )
+        }
+        else {
+            return (
+                <div>
+                    <Nav/>
+                    <div className="headerContainerBis">
+                        <h2>Compte Personnel</h2>
+                    </div>
+                    <div className='menuPerso'>
+                        <button className='buttonInfo' type='button' onClick={this.clickInfo}>Informations personnelles</button>
+                        <button className='selectFavorite' type='button' onClick={this.clickFavorite}>Favoris</button>
+                    </div>
+                    <Favorite/>
+                    {/* <div style={div}>
+                        <p style={logStyle}>Hello</p>
+                    </div> */}
+                    <Footer/>
+                </div>
+            )
+        }
     }
 }
-export default withRouter (Useraccount);
+export default withRouter (Useraccount);    
