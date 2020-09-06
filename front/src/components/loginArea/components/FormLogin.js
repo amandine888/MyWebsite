a3aae46635904d3e4f87a2678270ab40e04eee2f
Amandine <amandine2.divay@gmail.com>
2020-09-06 18:11:24 +0200

    front : rework login form

--- front/src/components/loginArea/components/FormLogin.js
+++ front/src/components/loginArea/components/FormLogin.js
@@ -1,10 +1,30 @@
-import React from 'react';
-import {withRouter} from 'react-router-dom';
+import React from "react";
+import {withRouter} from "react-router-dom";
 
 // import CSS : 
-import '../../../css/Mystyle.css'; 
-import './../css/styleForm.css'; 
+import "../../../css/Mystyle.css"; 
+import "./../css/styleForm.css"; 
 
+// Material UI : 
+import Button from "@material-ui/core/Button";
+import FormHelperText from "@material-ui/core/FormHelperText";
+import { styled } from '@material-ui/core/styles';
+
+// Style : 
+const logStyle = {
+    color : 'black',
+    width : '40vw',
+    height: '50px',
+    backgroundColor: '#B75D69',
+    fontWeight: "500",
+    padding: "1.5vh 0vw",
+    marginTop: "4vh"
+};
+
+const HelperText = styled(FormHelperText)({
+    color : "#B52E2C", 
+    fontSize: "0.7em",
+})
 
 class Login extends React.Component {
 
@@ -13,15 +33,14 @@ class Login extends React.Component {
 
         this.state = {
             email: "",
-            password: "",  
+            password: "", 
+            error: "" 
         }; 
 
         // bind : 
 
         this.handleChange = this.handleChange.bind(this); 
-        this.connect = this.connect.bind(this); 
-        // this.nameUser = this.nameUser.bind(this); 
-
+        this.connect = this.connect.bind(this);  
     }
 
     // props : 
@@ -39,9 +58,9 @@ class Login extends React.Component {
         
         const {email,password} = this.state; 
 
-        fetch ('http://localhost:3050/login', {
-            method: 'Post',
-            mode: 'cors', 
+        fetch ("http://localhost:3050/login", {
+            method: "POST",
+            mode: "cors", 
             body: JSON.stringify({
                 email, 
                 password
@@ -53,15 +72,15 @@ class Login extends React.Component {
         })
         .then((res) =>{
             if (res.status === 400) {
-                this.setState ({error: 'Mot de passe invalide'}); 
+                this.setState ({error: "Mot de passe invalide"}); 
             }
             else {
                 console.log(res); 
             }
             if (res.status === 200) {
                 res.json().then ((res) => {
-                    localStorage.setItem('token', res.token);
-                    this.props.history.push('/userAccount');
+                    localStorage.setItem("token", res.token);
+                    this.props.history.push("/userAccount");
                     console.log(res.token)
                 });
             }
@@ -69,31 +88,27 @@ class Login extends React.Component {
         .catch(error => console.log(error));
     }
 
-    // nameUser () {
-        
-    // }
-
     render () {
         return (
             <div>
-                <div className='formContainer'>
-                    <div className='titleForm'>
+                <form className="formContainer">
+                    <div className="titleForm">
                         <h2>Connexion</h2>
                     </div>
-                    <div className='fieldContainer'>
-                        <div className='fieldStyle'>
-                            <label className='styleLabel'>Email</label>
-                            <input className='styleInput' type='email' name='email' value = {this.state.email} onChange={this.handleChange} required></input>
+                    <div className="fieldContainer">
+                        <div className="fieldStyle">
+                            <label className="styleLabel">Email</label>
+                            <input className="styleInput" type="email" name="email" id="email" placeholder="Entrez votre email" value = {this.state.email} onChange={this.handleChange} required></input>
                         </div>
-                        <div className='fieldStyle'>
-                            <label className='styleLabel'>Password</label>
-                            <input className='styleInput' type='password' name='password' value = {this.state.password} onChange={this.handleChange} required></input>
+                        <div className="fieldStyle">
+                            <label className="styleLabel">Mot de passe</label>
+                            <input className="styleInput" type="password" name="password" id="password" placeholder="Entrez votre mot de passe" value = {this.state.password} onChange={this.handleChange} required></input>
+                            <HelperText id="component-error-text">{this.state.error}</HelperText>
                             <p className='textForm'>Mot de passe oublié ? Cliquez sur ce lien</p>
-                            <p>{this.state.error}</p>
                         </div>
-                        <button className='styleButton' onClick={this.connect}>Se connecter</button>
+                        <Button variant="contained" type="submit" onClick={this.connect} style={logStyle}>Se connecter</Button>
                     </div>
-                </div>
+                </form>
             </div>
         )
     }
